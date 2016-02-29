293e65379f1456f00b8e9413d1275d362272b786
= <travis@travis-ci.org>
2016-02-29 16:50:18 +0100

    Front End: Customizing

--- includes/header.php
+++ includes/header.php
@@ -1,90 +1,38 @@
-<header class="top_panel_wrap top_panel_style_3 scheme_original">
+    <header class="top_panel_wrap top_panel_style_3 scheme_original">
         <div class="top_panel_wrap_inner top_panel_inner_style_3 top_panel_position_above">
-
-                                        <div class="top_panel_top">
-                        <div class="content_wrap clearfix">
-
-<div class="top_panel_top_contact_phone icon-phone">1-800-64-38</div>
-
-<div class="top_panel_top_open_hours icon-clock">Monday – Friday 8.00 am – 5.00 pm, Weekend Closed</div>
-
-<div class="top_panel_top_contact_email"><a class="__cf_email__" href="cdn-cgi/l/email-protection.html" data-cfemail="4920272f260928272a263b283d212c242c3a672a2624">[email&#160;protected]</a><script data-cfhash='f9e31' type="text/javascript">
-/* <![CDATA[ */!function(){try{var t="currentScript"in document?document.currentScript:function(){for(var t=document.getElementsByTagName("script"),e=t.length;e--;)if(t[e].getAttribute("data-cfhash"))return t[e]}();if(t&&t.previousSibling){var e,r,n,i,c=t.previousSibling,a=c.getAttribute("data-cfemail");if(a){for(e="",r=parseInt(a.substr(0,2),16),n=2;a.length-n;n+=2)i=parseInt(a.substr(n,2),16)^r,e+=String.fromCharCode(i);e=document.createTextNode(e),c.parentNode.replaceChild(e,c)}t.parentNode.removeChild(t);}}catch(u){}}()/* ]]> */</script></div>
-
-<div class="top_panel_top_user_area">
-        <ul id="menu_user" class="menu_user_nav">
-
-</ul>
-
-</div>					</div>
+            <div class="top_panel_top">
+                <div class="content_wrap clearfix">
+                    <div class="top_panel_top_contact_phone icon-phone"><?php echo COMPANY_HOTLINE; ?></div>
+                    <div class="top_panel_top_open_hours icon-clock">Monday – Saturday 8.00 am – 5.00 pm</div>
+                    <div class="top_panel_top_contact_email">
+                        <a class="__cf_email__" href="mailto:<?php echo COMPANY_EMAIL; ?>"><?php echo COMPANY_EMAIL; ?></a>
+                    </div>
+                    <div class="top_panel_top_user_area">   <ul id="menu_user" class="menu_user_nav"> </ul> </div>					
                 </div>
+            </div>
 
-        <div class="top_panel_middle" >
+            <div class="top_panel_middle" >
                 <div class="content_wrap">
-                        <div class="columns_wrap columns_fluid"><div
-                                class="column-1_3 contact_logo">
-                                                        <div class="logo">
-        <a href="index.html"><img src="uploads/2015/11/nica_dark.png" class="logo_main" alt="" width="182" height="48"><img src="uploads/2015/11/nica_dark.png" class="logo_fixed" alt="" width="182" height="48"><div class="logo_slogan">Event Agency</div></a>
-</div>
-                                                </div><div 
-                                class="column-2_3 menu_main_wrap">
-                                        <a href="#" class="menu_main_responsive_button icon-menu"></a>
-                                        <nav class="menu_main_nav_area">
-                                                <ul id="menu_main" class="menu_main_nav"><li id="menu-item-792" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-792"><a href="#">Home</a>
-<ul class="sub-menu">
-<li id="menu-item-794" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-133 current_page_item menu-item-794"><a href="index.html">Home 1</a></li>
-<li id="menu-item-1096" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1096"><a href="home-page-2/index.html">Home 2</a></li>
-<li id="menu-item-1197" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1197"><a href="home-page-3/index.html">Home 3</a></li>
-<li id="menu-item-1236" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1236"><a href="home-page-4/index.html">Home 4</a></li>
-</ul>
-</li>
-<li id="menu-item-124" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-124"><a href="#">Features</a>
-<ul class="sub-menu">
-<li id="menu-item-795" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-795"><a href="shortcodes/index.html">Shortcodes</a></li>
-<li id="menu-item-796" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-796"><a href="typography/index.html">Typography</a></li>
-</ul>
-</li>
-<li id="menu-item-1313" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1313"><a href="#">About</a>
-<ul class="sub-menu">
-<li id="menu-item-1317" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1317"><a href="about-us/index.html">About us</a></li>
-<li id="menu-item-1315" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1315"><a href="team/index.html">Team</a></li>
-<li id="menu-item-1314" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1314"><a href="clients/index.html">Clients</a></li>
-<li id="menu-item-1316" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1316"><a href="contacts/index.html">Contacts</a></li>
-</ul>
-</li>
-<li id="menu-item-1399" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1399"><a href="services/index.html">Services</a></li>
-<li id="menu-item-804" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-804"><a href="#">Gallery</a>
-<ul class="sub-menu">
-<li id="menu-item-899" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-899"><a href="grid-gallery/index.html">Grid</a></li>
-<li id="menu-item-898" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-898"><a href="masonry-gallery/index.html">Masonry</a></li>
-<li id="menu-item-897" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-897"><a href="cobbles-gallery/index.html">Cobbles</a></li>
-</ul>
-</li>
-<li id="menu-item-799" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-799"><a href="#">Blog</a>
-<ul class="sub-menu">
-<li id="menu-item-1056" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1056"><a href="all-posts/index.html">Standard</a></li>
-<li id="menu-item-882" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-882"><a href="#">Masonry</a>
-<ul class="sub-menu">
-<li id="menu-item-884" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-884"><a href="category/masonry-2-columns/index.html">Masonry (2 columns)</a></li>
-<li id="menu-item-885" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-885"><a href="category/masonry-3-columns/index.html">Masonry (3 columns)</a></li>
-</ul>
-</li>
-<li id="menu-item-883" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-883"><a href="#">Portfolio</a>
-<ul class="sub-menu">
-<li id="menu-item-886" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-886"><a href="category/portfolio-2-columns/index.html">Portfolio (2 columns)</a></li>
-<li id="menu-item-887" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-887"><a href="category/portfolio-3-columns/index.html">Portfolio (3 columns)</a></li>
-</ul>
-</li>
-<li id="menu-item-888" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-888"><a href="category/post-formats/index.html">Post Formats</a></li>
-</ul>
-</li>
-<li id="menu-item-1578" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1578"><a href="shop/index.html">Shop</a></li>
-</ul>							</nav>
-
-                                </div>
+                    <div class="columns_wrap columns_fluid">
+                        <div class="column-1_3 contact_logo">
+                            <div class="logo">
+                                <a href="<?php echo SITE_URL; ?>"><img src="images/logo.jpg" class="logo_main" alt="" width="182" height="48"><img src="images/logo.jpg" class="logo_fixed" alt="" width="182" height="48"><div class="logo_slogan">Vien Patrick Events</div></a>
+                            </div>
+                        </div><div class="column-2_3 menu_main_wrap">
+                            <a href="#" class="menu_main_responsive_button icon-menu"></a>
+                            <nav class="menu_main_nav_area">
+                                <ul id="menu_main" class="menu_main_nav">
+                                    <li id="menu-item-792" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor menu-item-792"><a href="#">Home</a></li>
+                                    <li id="menu-item-124" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-124"><a href="#">Events</a></li>
+                                <li id="menu-item-1313" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1313"><a href="#">About</a></li>
+                                <li id="menu-item-1399" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1399"><a href="services/index.html">Services</a></li>
+                                <li id="menu-item-804" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-804"><a href="#">Gallery</a></li>
+                                <li id="menu-item-1578" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1578"><a href="shop/index.html">Contact Us</a></li>
+                                </ul>							
+                            </nav>
                         </div>
+                    </div>
                 </div>
+            </div>
         </div>
-
-        </div>
-</header>
+    </header>
